cf890f54de3af9f7bc23f61dad8fcaf6e8aceb77
Nestor Duarte <78217681+Antoduarte@users.noreply.github.com>
2021-05-21 19:30:42 -0600

    Update animar.js

--- js/animar.js
+++ js/animar.js
@@ -1,12 +1,12 @@
 
 
 window.addEventListener('scroll', function(){
-    var animado = document.querySelectorAll(".animar");
+    let animado = document.querySelectorAll(".animar");
     
-    for (var i=0; i < animado.length; i++){
+    for (let i=0; i < animado.length; i++){
        
-        var altura = window.innerHeight/1.8
-        var distancia = animado[i].getBoundingClientRect().top;
+        let altura = window.innerHeight/1.8
+        let distancia = animado[i].getBoundingClientRect().top;
        
         if(distancia <= altura){
             animado[i].style.opacity = 1;
